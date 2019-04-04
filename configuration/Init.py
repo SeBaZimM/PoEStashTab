61a681c981674f9c27b1cfbe06c404b4a641ab52
Sebastian <sebazimm48@gmail.com>
2019-04-04 23:28:23 +0200

    Removed asyncio at the moment, while it still be problems by implementing the gui changed the parameters from construct to clinet itself. Create method get_req_img to download the req images for the app. Implements an dynamic get stash download from a given url. Todo Tag has to make at soon as possible. get:stash_tabs the same like get_stash. global change with the path- no chdir is implement.

--- configuration/Init.py
+++ configuration/Init.py
@@ -3,124 +3,125 @@
 
 # IMPORTS
 import os
-import asyncio
-import browser_cookie3
+import json
 
 from utilities import utils
 from configuration import config
 
+
 class Init():
 
-	def __init__(self, account_name, league, cookie_jar):
-		self.account_name = account_name
-		self.league 	  = league
-		self.cookie_jar	  = cookie_jar
-		self.path 		  = os.getcwd()
-		self.num_tabs 	  = 0
-		self.chars 		  = list()
-		self.is_setup 	  = False
+	def __init__(self, client):
+		self.client		  		= client
+		self.path 		  		= os.getcwd()
+		self.num_tabs_synth 	= 0
+		self.num_tabs_hc_synth	= 0
+		self.num_tabs_stand		= 0
+		self.num_tabs_hc	  	= 0
+		self.chars 		  		= list()
+		self.is_setup 	  		= False
+		self.check_setup('logfile.log')
+
 
 	# Method: setup
 	# The setup method to start the setup
-	async def setup(self):
-		await self.check_setup('logfile.log')
+	def setup(self):
 		if self.is_setup:
 			return
-		await asyncio.gather(
-			self.set_config(config),
-			self.create_req_folders(config.REQUIRE_FOLDERS),
-			self.get_leagues(config.POE + config.LEAGUES, self.cookie_jar),
-			self.get_characters(config.POE + config.CHARACTERS, self.cookie_jar),
-			self.get_mtx_stash(config.POE + config.MTXSTASH, self.cookie_jar),
-			self.get_stash(config.POE + config.STASH, self.cookie_jar),
-			self.get_stash_tabs(config.POE + config.STASHTAB, self.cookie_jar),
-			self.get_chars_items(config.POE + config.ITEMS, self.cookie_jar),
-			self.get_chars_passives(config.POE + config.PASSIVES, self.cookie_jar)
-		)
+		self.set_config(config),
+		self.create_req_folders(config.REQUIRE_FOLDERS)
+		self.get_req_img(config.POE, config.REQUIRE_IMG)
+		self.get_leagues(config.POE + config.LEAGUES, self.client.cookie_jar)
+		self.get_characters(config.POE + config.CHARACTERS, self.client.cookie_jar)
+		self.get_mtx_stash(config.POE + config.MTXSTASH, self.client.cookie_jar)
+		self.get_stash(config.POE + config.STASH, self.client.cookie_jar)
+		self.get_stash_tabs(config.POE + config.STASHTAB, self.client.cookie_jar)
+		self.get_chars_items(config.POE + config.ITEMS, self.client.cookie_jar)
+		self.get_chars_passives(config.POE + config.PASSIVES, self.client.cookie_jar)
+
 
 	# Method: update_all
 	# Updates all data		
-	async def update_all(self):
-		await asyncio.gather(
-				self.get_leagues(config.POE + config.LEAGUES, self.cookie_jar),
-				self.get_characters(config.POE + config.CHARACTERS, self.cookie_jar),
-				self.get_mtx_stash(config.POE + config.MTXSTASH, self.cookie_jar),
-				self.get_stash(config.POE + config.STASH, self.cookie_jar),
-				self.get_stash_tabs(config.POE + config.STASHTAB, self.cookie_jar),
-				self.get_chars_items(config.POE + config.ITEMS, self.cookie_jar),
-				self.get_chars_passives(config.POE + config.PASSIVES, self.cookie_jar)
-			)
+	def update_all(self):
+		self.set_config(config)
+		self.get_leagues(config.POE + config.LEAGUES, self.client.cookie_jar)
+		self.get_characters(config.POE + config.CHARACTERS, self.client.cookie_jar)
+		self.get_mtx_stash(config.POE + config.MTXSTASH, self.client.cookie_jar)
+		self.get_stash(config.POE + config.STASH, self.client.cookie_jar)
+		self.get_stash_tabs(config.POE + config.STASHTAB, self.client.cookie_jar)
+		self.get_chars_items(config.POE + config.ITEMS, self.client.cookie_jar)
+		self.get_chars_passives(config.POE + config.PASSIVES, self.client.cookie_jar)
 
 
 	# Method: update_leagues
 	# Updates the PoE leagues
-	async def update_leagues(self):
-		await self.set_config(config)
-		await self.get_leagues(config.POE + config.LEAGUES, self.cookie_jar)
+	def update_leagues(self):
+		self.set_config(config)
+		self.get_leagues(config.POE + config.LEAGUES, self.client.cookie_jar)
 
 	# Method: update_characters
 	# Updates all characters
-	async def update_characters(self):
-		await self.set_config(config)
-		await self.get_characters(config.POE + config.CHARACTERS, self.cookie_jar)
+	def update_characters(self):
+		self.set_config(config)
+		self.get_characters(config.POE + config.CHARACTERS, self.client.cookie_jar)
 
 	# Method: update_mtxstash
 	# Updates the mtxstash
-	async def update_mtxstash(self):
-		await self.set_config(config)
-		await self.get_mtx_stash(config.POE + config.MTXSTASH, self.cookie_jar)
+	def update_mtxstash(self):
+		self.set_config(config)
+		self.get_mtx_stash(config.POE + config.MTXSTASH, self.client.cookie_jar)
 
 	# Method: update_stash
 	# Updates the stash
-	async def update_stash(self):
-		await self.set_config(config)
-		await self.get_stash(config.POE + config.STASH, self.cookie_jar)
+	def update_stash(self):
+		self.set_config(config)
+		self.get_stash(config.POE + config.STASH, self.client.cookie_jar)
 
 	# Method: update_stashtab
 	# Updates a specific stashtab
-	async def update_stashtab(self, tab):
-		await self.set_config(config)
-		await self.get_stash_tab(config.POE + config.STASHTAB, tab, self.cookie_jar)
+	def update_stashtab(self, tab):
+		self.set_config(config)
+		self.get_stash_tab(config.POE + config.STASHTAB, tab, self.client.cookie_jar)
 
 	# Method: update_stashtabs
 	# Updates all stashtabs
-	async def update_stashtabs(self):
-		await self.set_config(config)
-		await self.get_stash_tabs(config.POE + config.STASHTAB, self.cookie_jar)
+	def update_stashtabs(self):
+		self.set_config(config)
+		self.get_stash_tabs(config.POE + config.STASHTAB, self.client.cookie_jar)
 
 	# Method: update_char_items
 	# Updates all items from a character
 	#	Parameters:
 	#		char: the character name
 	#			type: str
-	async def update_char_items(self, char):
-		await self.set_config(config)
-		await self.get_char_items(config.POE + config.ITEMS, char, self.cookie_jar)
+	def update_char_items(self, char, league):
+		self.set_config(config)
+		self.get_char_items(config.POE + config.ITEMS, char, self.client.cookie_jar)
 
 	# Method: update_all_chars_items
 	# Updates all items from all characters
-	async def update_all_chars_items(self):
-		await self.set_config(config)
-		await self.get_chars_items(config.POE + config.ITEMS, self.cookie_jar)
+	def update_all_chars_items(self):
+		self.set_config(config)
+		self.get_chars_items(config.POE + config.ITEMS, self.client.cookie_jar)
 
 	# Method: update_char_passives
 	# Updates all passives from a character
 	#	Parameters:
 	#		char: the character name
 	#			type: str
-	async def update_all_chars_passives(self, char):
-		await self.set_config(config)
-		await self.get_char_passives(config.POE + config.PASSIVES, char, self.cookie_jar)
+	def update_all_chars_passives(self, char, league):
+		self.set_config(config)
+		self.get_char_passives(config.POE + config.PASSIVES, char, self.client.cookie_jar)
 
 	# Method: update_all_chars_passives
 	# Updates all passives from all characters	
-	async def update_all_chars_passives(self):
-		await self.set_config(config)
-		await self.get_chars_passives(config.POE + config.PASSIVES, self.cookie_jar)
+	def update_all_chars_passives(self):
+		self.set_config(config)
+		self.get_chars_passives(config.POE + config.PASSIVES, self.client.cookie_jar)
 		
 
 	# Method: check_setup
-	# Checked if a setup was already taken
+	# Checking if a setup was already taken
 	#	Parameters:
 	#		logfile: the logfile to check / create
 	#			type: str
@@ -128,7 +129,7 @@ class Init():
 	#			type: bool
 	#	Return:
 	#		is_setup
-	async def check_setup(self, logfile):
+	def check_setup(self, logfile):
 		if not os.path.isfile(self.path +'/'+ logfile):
 			with open(logfile, 'w') as log:
 				log.write('###################################################################\n')
@@ -136,39 +137,64 @@ class Init():
 		else: 
 			self.is_setup = True
 			return self.is_setup
-		#if not os.stat(path +'/'+ logfile).st_size is 0:
+
 
 	# Method: set_config
 	# Sets the information in the config
 	#	Parameters:
 	#		conf: the configuration file
 	#			type: module 
-	async def set_config(self, conf):
+	def set_config(self, conf):
 		conf.ROOTPATH = self.path
-		conf.ACCOUNTNAME = self.account_name
-		conf.LEAGUE = self.league
+		conf.ACCOUNTNAME = self.client.account_name
+		conf.LEAGUE = self.client.league
+
 
 	# Method: create_req_folders
 	# Creates the required folders given from the config
 	#	Parameters:
 	#		req_folders: the requiered folders
 	#			type: list
-	async def create_req_folders(self, req_folders):
+	def create_req_folders(self, req_folders):
 		for req_folder in req_folders:
-			if not os.path.exists(os.getcwd() +'/'+ req_folder):
+			if not os.path.exists(self.path +'/'+ req_folder):
 				os.mkdir(self.path +'/'+ req_folder)
 
+
+	# Method: get_req_img
+	# Downloads the required images given from the config
+	#	Parameters:
+	#		conf_url: the given url 
+	#			type: str
+	#		img_urls: the requiered images
+	#			type: list
+	def get_req_img(self, conf_url, img_urls):
+		for img_url in img_urls:
+			if 'synthesis' in img_url:
+				filename_pos_start = img_url.find('/image/layout/') + len('/image/layout/')
+			elif 'tabs-control' in img_url:
+				filename_pos_start = img_url.find('/image/inventory/tabs-control/') + len('/image/inventory/tabs-control/')
+			else:
+				filename_pos_start = img_url.find('/image/inventory/') + len('/image/inventory/')
+			filename_pos_end = img_url.find('?')
+			filename = img_url[ filename_pos_start : filename_pos_end]
+			if os.path.exists(self.path +'/ressources/images/inventory/'+ filename):
+				continue
+			response = utils.start_request(conf_url + img_url, None)
+			utils.save_img(filename, response, conf_url)
+
+
 	# Method: get_leagues
-	# Get all leages from PoE
+	# Get all leagues from PoE
 	#	Parameters:
 	#		conf_url: the given url 
 	#			type: str
 	#		cookie_jar: the saved session cookie
 	#			type: cookie_jar
-	async def get_leagues(self, conf_url, cookie_jar):
-		os.chdir(self.path +'/ressources')
+	def get_leagues(self, conf_url, cookie_jar):
 		response = utils.start_request(conf_url, cookie_jar)
-		utils.save_dump('leagues.json', response, conf_url)
+		utils.save_dump(self.path +'/ressources/leagues.json', response, conf_url)
+
 
 	# Method: get_characters
 	# Get all the characters from account
@@ -177,12 +203,12 @@ class Init():
 	#			type: str
 	#		cookie_jar: the saved session cookie
 	#			type: cookie_jar
-	async def get_characters(self, conf_url, cookie_jar):
-		os.chdir(self.path +'/ressources')
+	def get_characters(self, conf_url, cookie_jar):
 		response = utils.start_request(conf_url, cookie_jar)
 		for char in response: self.chars.append(char)
-		utils.save_dump('characters.json', response, conf_url)
+		utils.save_dump(self.path +'/ressources/characters/characters.json', response, conf_url)
 
+	
 	# Method: get_stash
 	# Get all the stash from account
 	#	Parameters:
@@ -190,25 +216,49 @@ class Init():
 	#			type: str
 	#		cookie_jar: the saved session cookie
 	#			type: cookie_jar
-	async def get_stash(self, conf_url, cookie_jar):
-		os.chdir(self.path +'/ressources')
-		conf_url = utils.replace_placeholder(conf_url, self.account_name, self.league)
-		response = utils.start_request(conf_url, cookie_jar)
-		self.num_tabs = response['numTabs']
-		utils.save_dump('stash.json', response, conf_url)
+	def get_stash(self, conf_url, cookie_jar):
+		with open(self.path +'/ressources/leagues.json','r') as leagues:
+			j_leagues = json.load(leagues)
+			for league in j_leagues['result']:
+				url_tmp = conf_url
+				conf_url = utils.replace_placeholder(conf_url, self.client.account_name, league['id'])
+				response = utils.start_request(conf_url, cookie_jar)
+				if league['id'] == 'Synthesis':
+					self.num_tabs_synth = response['numTabs']
+					if not os.path.exists(self.path +'/ressources/stashTabs/'+ league['id']):
+						os.mkdir(self.path +'/ressources/stashTabs/'+ league['id'])
+				elif league['id'] == 'Hardcore Synthesis':
+					self.num_tabs_hc_synth = response['numTabs']
+					if not os.path.exists(self.path +'/ressources/stashTabs/'+ league['id']):
+						os.mkdir(self.path +'/ressources/stashTabs/'+ league['id'])
+				elif league['id'] == 'Standard':
+					self.num_tabs_stand = response['numTabs']
+					if not os.path.exists(self.path +'/ressources/stashTabs/'+ league['id']):
+						os.mkdir(self.path +'/ressources/stashTabs/'+ league['id'])
+				elif league['id'] == 'Hardcore':
+					self.num_tabs_hc = response['numTabs']
+					if not os.path.exists(self.path +'/ressources/stashTabs/'+ league['id']):
+						os.mkdir(self.path +'/ressources/stashTabs/'+ league['id'])
+				utils.save_dump(self.path +'/ressources/stashTabs/stash_'+ league['id'] +'.json', response, conf_url)
+				conf_url = url_tmp
 
+
+	# TODO --------------------------------------
 	# Method: get_stash_tab
 	# Get one specific stashtab from the stash
 	#	Parameters:
 	#		conf_url: the given url 
 	#			type: str
+	#		tab: the specific stashtab
+	#			type: str / int
 	#		cookie_jar: the saved session cookie
 	#			type: cookie_jar
-	async def get_stash_tab(self, conf_url, tab, cookie_jar):
-		conf_url = utils.replace_placeholder(conf_url, self.account_name, self.league)
+	def get_stash_tab(self, conf_url, tab, cookie_jar):
+		conf_url = utils.replace_placeholder(conf_url, self.client.account_name, self.client.league)
 		os.chdir(self.path +'/ressources/stashTabs')
 		response = utils.start_request(conf_url + str(tab), cookie_jar)
 		utils.save_dump('stashtab'+str(tab)+'.json', response, conf_url + str(tab))
+
 
 	# Method: get_stash_tabs
 	# Get all the stashtabs from the stash
@@ -217,12 +267,21 @@ class Init():
 	#			type: str
 	#		cookie_jar: the saved session cookie
 	#			type: cookie_jar
-	async def get_stash_tabs(self, conf_url, cookie_jar):
-		conf_url = utils.replace_placeholder(conf_url, self.account_name, self.league)
-		for tab in range(self.num_tabs):
-			os.chdir(self.path +'/ressources/stashTabs')
-			response = utils.start_request(conf_url + str(tab), cookie_jar)
-			utils.save_dump('stashtab'+str(tab)+'.json', response, conf_url + str(tab))
+	def get_stash_tabs(self, conf_url, cookie_jar):
+		with open(self.path +'/ressources/leagues.json','r') as leagues:
+			j_leagues = json.load(leagues)
+			for league in j_leagues['result']:
+				url_tmp = conf_url
+				conf_url = utils.replace_placeholder(conf_url, self.client.account_name, league['id'])
+				if league['id']   == 'Synthesis': 		   numTabs = self.num_tabs_synth
+				elif league['id'] == 'Hardcore Synthesis': numTabs = self.num_tabs_hc_synth
+				elif league['id'] == 'Standard': 		   numTabs = self.num_tabs_stand 
+				elif league['id'] == 'Hardcore': 		   numTabs = self.num_tabs_hc 
+				for tab in range(numTabs):
+					response = utils.start_request(conf_url + str(tab), cookie_jar)
+					utils.save_dump(self.path +'/ressources/stashTabs/' + league['id'] +'/stashtab'+str(tab)+'.json', response, conf_url + str(tab))
+				conf_url = url_tmp
+
 
 	# Method: get_mtx_stash
 	# Get the mtx stash from account
@@ -231,25 +290,31 @@ class Init():
 	#			type: str
 	#		cookie_jar: the saved session cookie
 	#			type: cookie_jar
-	async def get_mtx_stash(self, conf_url, cookie_jar):
-		os.chdir(self.path +'/ressources')
+	def get_mtx_stash(self, conf_url, cookie_jar):
 		response = utils.start_request(conf_url, cookie_jar)
-		utils.save_dump('mtxstash.json', response, conf_url)
+		utils.save_dump(self.path +'/ressources/stashTabs/mtxstash.json', response, conf_url)
 
+
+	# TODO --------------------------------------
 	# Method: get_char_items
 	# Get all the items from a character
 	#	Parameters:
 	#		conf_url: the given url 
 	#			type: str
+	#		char: the charactername
+	#			type: str
+	#		league: the league
+	#			type: str
 	#		cookie_jar: the saved session cookie
 	#			type: cookie_jar
-	async def get_char_items(self, conf_url, char, cookie_jar):
+	def get_char_items(self, conf_url, char, league, cookie_jar):
 		os.chdir(self.path +'/ressources/characters/')
-		if not os.path.exists(os.getcwd() +'/'+ char):
+		if not os.path.exists(os.getcwd() +'/'+ league +'/'+ char):
 			os.mkdir(char)
-		os.chdir(self.path +'/ressources/characters/'+ char)
+		os.chdir(self.path +'/ressources/characters/'+ league +'/'+  char)
 		response = utils.start_request(conf_url + char, cookie_jar)
 		utils.save_dump('items.json', response, conf_url + char)
+
 
 	# Method: get_chars_items
 	# Get all the items from all characters
@@ -258,29 +323,33 @@ class Init():
 	#			type: str
 	#		cookie_jar: the saved session cookie
 	#			type: cookie_jar
-	async def get_chars_items(self, conf_url, cookie_jar):
+	def get_chars_items(self, conf_url, cookie_jar):
 		for char in self.chars:
-			os.chdir(self.path +'/ressources/characters/')
-			if not os.path.exists(os.getcwd() +'/'+ char['name']):
-				os.mkdir(char['name'])
-			os.chdir(self.path +'/ressources/characters/'+ char['name'])
+			if not os.path.exists(self.path +'/ressources/characters/'+ char['league']):
+				os.mkdir(self.path +'/ressources/characters/'+ char['league'])
+			if not os.path.exists(self.path +'/ressources/characters/'+ char['league'] +'/'+ char['name']):
+				os.mkdir(self.path +'/ressources/characters/'+ char['league'] +'/'+ char['name'])
 			response = utils.start_request(conf_url + char['name'], cookie_jar)
-			utils.save_dump('items.json', response, conf_url + char['name'])
+			utils.save_dump(self.path +'/ressources/characters/'+ char['league'] +'/items.json', response, conf_url + char['name'])
+
 
 	# Method: get_char_passives
 	# Get all the passives from a character
 	#	Parameters:
 	#		conf_url: the given url 
 	#			type: str
+	#		char: the charactername
+	#			type: str
+	#		league: the league
+	#			type: str
 	#		cookie_jar: the saved session cookie
 	#			type: cookie_jar
-	async def get_char_passives(self, conf_url, char, cookie_jar):
-		os.chdir(self.path +'/ressources/characters/')
-		if not os.path.exists(os.getcwd() +'/'+ char):
-			os.mkdir(char)
-		os.chdir(self.path +'/ressources/characters/'+ char)
+	def get_char_passives(self, conf_url, char, league, cookie_jar):
+		if not os.path.exists(self.path +'/ressources/characters/'+ league +'/'+ char):
+			os.mkdir(self.path +'/ressources/characters/'+ league +'/'+ char)
 		response = utils.start_request(conf_url + char, cookie_jar)
-		utils.save_dump('pasives.json', response, conf_url + char)
+		utils.save_dump(self.path +'/ressources/characters/'+ league +'/'+ char +'/pasives.json', response, conf_url + char)
+
 
 	# Method: get_chars_passives
 	# Get all the passives from all characters
@@ -289,11 +358,11 @@ class Init():
 	#			type: str
 	#		cookie_jar: the saved session cookie
 	#			type: cookie_jar
-	async def get_chars_passives(self, conf_url, cookie_jar):
+	def get_chars_passives(self, conf_url, cookie_jar):
 		for char in self.chars:
-			os.chdir(self.path +'/ressources/characters/')
-			if not os.path.exists(os.getcwd() +'/'+ char['name']):
-				os.mkdir(char['name'])
-			os.chdir(self.path +'/ressources/characters/'+ char['name'])
+			if not os.path.exists(self.path +'/ressources/characters/'+ char['league']):
+				os.mkdir(self.path +'/ressources/characters/'+ char['league'])
+			if not os.path.exists(self.path +'/ressources/characters/'+ char['league'] +'/'+ char['name']):
+				os.mkdir(self.path +'/ressources/characters/'+ char['league'] +'/'+ char['name'])
 			response = utils.start_request(conf_url + char['name'], cookie_jar)
-			utils.save_dump('pasives.json', response, conf_url + char['name'])
+			utils.save_dump(self.path +'/ressources/characters/'+ char['league'] +'/'+ char['name'] +'/pasives.json', response, conf_url + char['name'])
