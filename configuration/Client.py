544d2f5efa8b745d5d0794bd11832b107e4d9ef4
Sebastian <sebazimm48@gmail.com>
2019-04-04 23:27:20 +0200

    Added Client method to modifing name and league

--- configuration/Client.py
+++ configuration/Client.py
@@ -11,3 +11,11 @@ class Client():
 		self.account_name = account_name
 		self.league 	  = league
 		self.cookie_jar	  = browser_cookie3.load(config.POE[8:])
+
+
+	def change_account_name(self, account_name):
+		self.account_name = account_name
+
+
+	def change_league(self, league):
+		self.league = league
