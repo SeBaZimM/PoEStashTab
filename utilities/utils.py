54ea5d3fd1119f1ef34a89eaaa012f94f6f9f7be
Sebastian <sebazimm48@gmail.com>
2019-04-04 23:36:26 +0200

    Canged some commmit to globaly description. Implements save_img function for saving the req images for the app.

--- utilities/utils.py
+++ utilities/utils.py
@@ -9,27 +9,33 @@ from configuration import config
 
 
 # Function: start_request
-# Request the data from the given account /league and the ressources
+# Start a request from a given url
 #	Parameters:
 #		url: the given url to start a request
 #			type: str
 #		cookie: the saved session cookie from PoE
 #			type: cookie_jar
+#	Return:
+#		res_json: an json object including the information
+#			type: json
 def start_request(url, cookie):
 	try:
 		response = requests.get(url, cookies=cookie)
 		if response.status_code == 200:
 			if not response is None:
-				res_json = json.loads(response.text)
-				return res_json
+				if not cookie is None:
+					res_json = json.loads(response.text)
+					return res_json
+				else:
+					return response
 		else:
 			raise Exception(response)
 	except Exception as ex:
-		print('ERROR: {}'.format(ex))
+		print('ERROR: {}, {}'.format(ex, url))
+
 
 # Function: save_dump
-# Saved the league and characters ressources 
-# whithout given account/league informations
+# Saved the given information as an json object
 #	Parameters:
 #		filename: the filename to save
 #			type: str
@@ -38,31 +44,46 @@ def start_request(url, cookie):
 #		ressources: the url thet was requested
 #			type: str 
 def save_dump(filename, res_json, ressource):
+	if res_json is None:
+		return
 	with open(filename, 'w') as json_file:
 		json.dump(res_json, json_file, indent=2)
-		write_logfile('logfile.log', os.getcwd(), filename, ressource)
+		write_logfile('logfile.log', filename, ressource)
+
+
+# Function: save_img
+# Saved the given information as an json object
+#	Parameters:
+#		filename: the filename to save
+#			type: str
+#		res_json: the response json object
+#			type: json
+#		ressources: the url thet was requested
+#			type: str 
+def save_img(filename, response, ressource):
+	with open(os.getcwd() +'/ressources/images/inventory/' + filename, 'wb') as img:
+		img.write(response.content)
+		write_logfile('logfile.log', filename, ressource)
+
 
 # Function: write_logfile
-# Saved the league and characters ressources 
-# whithout given account/league informations
+# Write the given parameters into the logfile
 #	Parameters:
 #		logfile: the logfile to write 	 
-#			type: str
-#		dest_path: the information where the file was saved 	 
 #			type: str
 #		filename: the filename that was saved 	 
 #			type: str
 #		ressources: the url from where the information is 	 
 #			type: str
-def write_logfile(logfile, dest_path, filename, ressource):
-	os.chdir(config.ROOTPATH)
+def write_logfile(logfile, filename, ressource):
 	with open(logfile, 'a+') as log:
-		output = '[PATH]{}\n[FILE]{}\n[RESSOURCE]{}\n'.format(
-			dest_path, filename, ressource)
+		output = '[FILE]{}\n[RESSOURCE]{}\n'.format(
+			filename, ressource)
 		log.write(output + '###################################################################\n')
 
+
 # Function: replace_placeholder
-# Replace the standart ACCOUNTNAME / LEAGUE in the url
+# Replace the default ACCOUNTNAME / LEAGUE in the url
 #	Parameters:
 #		url: the given url to replace 	  
 #			type: str
